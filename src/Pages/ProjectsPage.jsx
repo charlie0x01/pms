1500c1179a0ef4867b7e675654756f97714c6e1c
Abdullah Tanveer <72660632+charlie0x01@users.noreply.github.com>
2023-03-16 14:57:03 +0500

    feat: add button with drawer component instead of button with modal

--- src/Pages/ProjectsPage.jsx
+++ src/Pages/ProjectsPage.jsx
@@ -1,7 +1,10 @@
 import React from "react";
-import CreateNewProject from "../components/CreateNewButton/CreateNewProject";
+import ButtonWithDrawer from "../components/common/ButtonWithDrawer";
 import FlexCard from "../components/common/FlexCard";
 import ProjectCard from "../components/common/ProjectCard";
+
+// antd
+import { Button, Form, Input } from "antd";
 
 const members = [
   "https://joesch.moe/api/v1/random?key=1",
@@ -13,7 +16,7 @@ const members = [
 const ProjectsPage = () => {
   return (
     <FlexCard>
-      <CreateNewProject />
+      <ButtonWithDrawer drawerTitle="Create New Project" buttonStyles={{ width: 300, height: "185px"}} buttonChildren={<p>Create New Project</p>} />
       <ProjectCard
         createdDate="10 Mar 2023"
         title="Project Managment System"
