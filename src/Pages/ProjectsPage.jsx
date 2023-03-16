936c9cb1b70a21794db6c029d06adc699d1b6de3
Abdullah Tanveer <72660632+charlie0x01@users.noreply.github.com>
2023-03-16 16:18:35 +0500

    feat: project have it's own drawer to add or edit project details

--- src/Pages/ProjectsPage.jsx
+++ src/Pages/ProjectsPage.jsx
@@ -1,10 +1,9 @@
 import React from "react";
-import ButtonWithDrawer from "../components/common/ButtonWithDrawer";
 import FlexCard from "../components/common/FlexCard";
-import ProjectCard from "../components/common/ProjectCard";
+import ProjectCard from "../components/common/DetailCard";
+import { useState } from "react";
 
-// antd
-import { Button, Form, Input } from "antd";
+import { Drawer, Button, message } from "antd";
 
 const members = [
   "https://joesch.moe/api/v1/random?key=1",
@@ -14,40 +13,76 @@ const members = [
 ];
 
 const ProjectsPage = () => {
+  const [isDrawerOpen, setIsDrawerOpen] = useState(false);
+  const [isEdit, setIsEdit] = useState(false);
+
+  const handleEdit = () => {
+    setIsEdit(true);
+    setIsDrawerOpen(true);
+  };
   return (
-    <FlexCard>
-      <ButtonWithDrawer drawerTitle="Create New Project" buttonStyles={{ width: 300, height: "185px"}} buttonChildren={<p>Create New Project</p>} />
-      <ProjectCard
-        createdDate="10 Mar 2023"
-        title="Project Managment System"
-        team={members}
-        projectProfile="https://joesch.moe/api/v1/random?key=1"
-      />
-      <ProjectCard
-        createdDate="10 Mar 2023"
-        title="Project Managment System"
-        team={members}
-        projectProfile="https://joesch.moe/api/v1/random?key=1"
-      />
-      <ProjectCard
-        createdDate="10 Mar 2023"
-        title="Project Managment System"
-        team={members}
-        projectProfile="https://joesch.moe/api/v1/random?key=1"
-      />
-      <ProjectCard
-        createdDate="10 Mar 2023"
-        title="Project Managment System"
-        team={members}
-        projectProfile="https://joesch.moe/api/v1/random?key=1"
-      />
-      <ProjectCard
-        createdDate="10 Mar 2023"
-        title="Project Managment System"
-        team={members}
-        projectProfile="https://joesch.moe/api/v1/random?key=1"
-      />
-    </FlexCard>
+    <>
+      <FlexCard>
+        <Button
+          type="dashed"
+          style={{ width: 350, height: 185 }}
+          onClick={() => setIsDrawerOpen(true)}
+        >
+          Create New Project
+        </Button>
+        <ProjectCard
+          id={1}
+          createdDate="10 Mar 2023"
+          title="Project Managment System"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          team={members}
+          projectProfile="https://joesch.moe/api/v1/random?key=1"
+          onEdit={handleEdit}
+        />
+        <ProjectCard
+          id={2}
+          createdDate="10 Mar 2023"
+          title="Project Managment System"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          team={members}
+          projectProfile="https://joesch.moe/api/v1/random?key=1"
+        />
+        <ProjectCard
+          id={3}
+          createdDate="10 Mar 2023"
+          title="Project Managment System"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          team={members}
+          projectProfile="https://joesch.moe/api/v1/random?key=1"
+        />
+        <ProjectCard
+          id={4}
+          createdDate="10 Mar 2023"
+          title="Project Managment System"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          team={members}
+          projectProfile="https://joesch.moe/api/v1/random?key=1"
+        />
+        <ProjectCard
+          id={5}
+          createdDate="10 Mar 2023"
+          title="Project Managment System"
+          description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+          team={members}
+          projectProfile="https://joesch.moe/api/v1/random?key=1"
+        />
+      </FlexCard>
+      <Drawer
+        width={500}
+        title={isEdit ? "Edit Project" : "Create New Project"}
+        open={isDrawerOpen}
+        closable
+        onClose={() => {
+          setIsEdit(false);
+          setIsDrawerOpen(false);
+        }}
+      ></Drawer>
+    </>
   );
 };
 export default ProjectsPage;
