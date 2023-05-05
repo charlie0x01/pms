e86ad782d8f1fdc3cd09f2fea0e303a455196a14
Abdullah Tanveer <72660632+charlie0x01@users.noreply.github.com>
2023-05-05 11:56:47 +0500

    Revert "✨feat: add user avatar dropdown in navbar and separated the navbar components"
    This reverts commit 521f2ea6eff8c5d3eec028916b1ed9e7185e082a.

--- frontend/src/Pages/layout/Layout.jsx
+++ frontend/src/Pages/layout/Layout.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Navbar from "../../components/common/Navbar";
+import Navbar from "../../components/Navbar/Navbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import { Outlet } from "react-router-dom";
 
