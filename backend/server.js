656e96523ee15a9970851eaecf1d434808374178
Abdullah Tanveer <72660632+charlie0x01@users.noreply.github.com>
2023-05-05 11:46:57 +0500

    🐛fix: add cors package to enable cross side scripting

--- backend/server.js
+++ backend/server.js
@@ -2,10 +2,16 @@ require("dotenv").config({ path: "./.env" });
 PORT = process.env.PORT || 5000;
 
 const express = require("express");
+const cors = require("cors");
 const app = express();
+
+// user auth routes
 const authRoutes = require("./routes/auth");
 
+app.use(cors());
 app.use(express.json());
+
+// 
 app.use("/api/auth", authRoutes);
 
 const server = app.listen(PORT, () => {
