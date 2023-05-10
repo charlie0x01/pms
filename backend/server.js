a988be5e9957401eecf2f465f61eb164be20b3bb
Abdullah Tanveer <72660632+charlie0x01@users.noreply.github.com>
2023-05-10 13:38:04 +0500

    ✨feat: add project apis

--- backend/server.js
+++ backend/server.js
@@ -2,17 +2,21 @@ require("dotenv").config({ path: "./.env" });
 PORT = process.env.PORT || 5000;
 
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const app = express();
 
 // user auth routes
-const authRoutes = require("./routes/auth");
+const authRoutes = require("./routes/auth.routes");
+const orgRoutes = require("./routes/organization.routes");
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 
-// 
+//
 app.use("/api/auth", authRoutes);
+app.use("/api/org", orgRoutes);
 
 const server = app.listen(PORT, () => {
   console.log("Server started on port 5000");
