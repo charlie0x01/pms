025596246ed12a1a6dac441951514916653c52fa
Hamza Khalid <hamzakh827@gmail.com>
2023-05-15 11:26:41 +0500

    Added Organization & project

--- backend/controllers/organization.controller.js
+++ backend/controllers/organization.controller.js
@@ -13,43 +13,6 @@ exports.addOrganization = async (req, res, next) => {
     // create new organization
     const org = new organization(organizationName, user[0].user_id);
     org.save();
-
-    return res.status(201).json({
-      success: true,
-      message: "organization successfully created",
-    });
-  } catch (error) {
-    res.status(500).json({ success: false, message: error?.message });
-  }
-};
-
-exports.getOrganization = async (req, res, next) => {
-  try {
-    const owner = req.params.owner;
-    const [orgs, _] = await organization.findByOwner(owner);
-    return res.status(200).json({
-      success: true,
-      data: [...orgs],
-    });
-  } catch (error) {
-    throw error;
-  }
-};
-const User = require("../models/user.model");
-const organization = require("../models/organization.model");
-
-exports.addOrganization = async (req, res, next) => {
-  try {
-    const { organizationName, email } = req.body;
-
-    // check
-    if (organizationName === "" || email === "")
-      res.json({ success: false, message: "required data not provided" });
-
-    const [user, _] = await User.findByEmailId(email);
-    // create new organization
-    const org = new organization(organizationName, user[0].user_id);
-    org.save();
     
     return res.status(201).json({
       success: true,
--- backend/routes/organization.routes.js
+++ backend/routes/organization.routes.js
@@ -3,19 +3,6 @@ const express = require("express");
 const {
   addOrganization,
   getOrganization,
-} = require("../controllers/organization.controller");
-
-const router = express.Router();
-
-router.post("/add-organization", addOrganization);
-router.get("/get-organizations/:owner", getOrganization);
-
-module.exports = router;
-const express = require("express");
-
-const {
-  addOrganization,
-  getOrganization,
   updateOrganization,
   deleteOrganization,
 } = require("../controllers/organization.controller");
