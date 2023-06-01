fdb9c42f19a91d6f005bc1a78ed2e5e55a3e6efa
Hamza Khalid <hamzakh827@gmail.com>
2023-06-02 02:45:35 +0500

    Added Forget Password API
    - Forget Password
    - Verify OTP
    - Reset Password

--- backend/routes/auth.routes.js
+++ backend/routes/auth.routes.js
@@ -2,18 +2,29 @@ const express = require("express");
 
 const {
   signup,
-  login,
-  forgetpassword,
   verifyEmail,
   newVerificationCode,
+  login,
+  forgetPassword,
+  verifyOTP,
+  resetPassword,
 } = require("../controllers/auth.controller");
 
 const router = express.Router();
 
+// Registration
 router.post("/signup", signup);
-router.post("/login", login);
-router.get("/forgetpassword", forgetpassword);
+
+// User Verification
 router.post("/verifyemail", verifyEmail);
 router.post("/new-verification-code", newVerificationCode);
+
+// Login
+router.post("/login", login);
+
+// Forget or Reset Password
+router.post("/forget-password", forgetPassword);
+router.post("/verify-otp", verifyOTP);
+router.post("/reset-password", resetPassword);
 
 module.exports = router;
