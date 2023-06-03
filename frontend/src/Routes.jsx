d4d2a1eab4f0f557e4cd5272b971fef43986b78c
Abdullah Tanveer <72660632+charlie0x01@users.noreply.github.com>
2023-06-03 14:54:08 +0500

    fix: verify email page not found fixed

--- frontend/src/Routes.jsx
+++ frontend/src/Routes.jsx
@@ -8,6 +8,7 @@ import ForgetPassword from "./pages/auth/ForgetPassword";
 import UserProfileSettings from "./components/User/UserProfileSettings";
 import UserProfile from "./components/User/UserProfile";
 import ProjectProfile from "./components/Project/ProjectProfile";
+import VerifyEmail from "./pages/auth/VerifyEmail";
 import ProjectView from "./components/Organization/ProjectView";
 import Board from "./components/Kanban/Board";
 import ProtectedRoute from "./components/common/ProtectedRoute";
@@ -32,6 +33,8 @@ const AppRoutes = () => {
       <Route path="/signin" element={<Login />} />
       <Route path="/signup" element={<SignUp />} />
       <Route path="/forget-password" element={<ForgetPassword />} />
+      <Route path="/verify-email" element={<VerifyEmail />} />
+
       <Route path="*" element={<_404Page />} />
     </Routes>
   );
